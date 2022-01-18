f3d5b273a7f10e4e68996f4da6dea22445121ac3
geovpn <96859296+geovpn@users.noreply.github.com>
2022-01-18 14:41:40 +0800

    Add files via upload

--- ssh/member.sh
+++ ssh/member.sh
@@ -2,8 +2,18 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/sampiiiiu"
+exit 0
+fi
 clear
 echo "---------------------------------------------------"
 echo "USERNAME          EXP DATE          STATUS"
