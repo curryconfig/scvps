9ec269d1463124dea2ce5032c630097ae5f82451
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-21 19:36:34 +0800

    Update geo.sh

--- geo.sh
+++ geo.sh
@@ -52,7 +52,7 @@ read -p "        Silahkan berikan jawaban anda : " jawab
 echo -e ""
 case $jawab in
 1)
-rm -f setup.sh && apt update && apt upgrade -y && apt install lolcat -y && gem install lolcat && apt install figlet -y && update-grub && sleep 2 && apt-get update -y && apt-get upgrade && sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl && wget https://raw.githubusercontent.com/geovpn/scvps/main/setup.sh && chmod +x setup.sh && screen -S setup ./setup.sh
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update &&  apt install lolcat -y && gem install lolcat && apt install toilet -y && apt install figlet -y && apt install -y bzip2 gzip coreutils screen curl && wget https://raw.githubusercontent.com/geovpn/scvps/main/setup.sh && chmod +x setup.sh && sed -i -e 's/\r$//' setup.sh && screen -S setup ./setup.sh
 ;;
 x)
 exit
